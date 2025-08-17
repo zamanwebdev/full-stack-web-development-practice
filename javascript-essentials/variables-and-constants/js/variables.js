dc88321be761c3cfd2ba2cb15cf4ba85ebdf428a
Syed Zaman <zamanwebdev@gmail.com>
2025-08-17 19:07:06 +0600

    76. Variables and Constants >> Practice = Done.

---
+++ javascript-essentials/variables-and-constants/js/variables.js
@@ -0,0 +1,37 @@
+// How to create variables 
+
+/*
+3 types of variables 
+
+1. Var : Old Style (Not Used)
+
+2. Let 
+
+3. Const 
+
+
+
+*/
+// 1. Using Var 
+var name = "Mamun"
+name = "Joy"
+
+console.log(name)
+
+// 2. Using Let (When Value Change)  
+
+let age = 25;
+console.log(age)
+
+
+
+age = 30
+
+console.log(age)
+// 3. Using Const (Used for fixed value ) 
+const Country = "Bangladesh"
+const Country2 = "USA"
+console.log(Country)
+console.log(Country2)
+
+
